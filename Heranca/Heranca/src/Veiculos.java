6646d45afa6e72d825470d2c1912866f988b3f29
Lucas Gabriel <123906808+lucasgabrielrs@users.noreply.github.com>
2024-04-25 18:21:12 -0300

    Add files via upload

---
+++ Heranca/Heranca/src/Veiculos.java
@@ -0,0 +1,13 @@
+public class Veiculos {
+
+  private int idVeiculo;
+  private String fabricante;
+  private String modelo;
+  private String placa;
+  private String potencia;
+  private int QtdePortas;
+  private String cor;
+  private String chassi;
+  private String renavam;
+
+}
